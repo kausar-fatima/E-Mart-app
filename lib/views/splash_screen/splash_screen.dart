f4ea3afcc1b6f4f4123d40823d37a95b89cf3971
Kausar Fatima <165108114+kausar-fatima@users.noreply.github.com>
2024-06-26 11:18:01 +0500

    project started

---
+++ lib/views/splash_screen/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:e_mart_app/consts/consts.dart';
+import 'package:e_mart_app/widgets_common/applogo_widget.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Image.asset(
+                icSplashBg,
+                width: 300,
+              ),
+            ),
+            20.heightBox,
+            applogoWidget(),
+            10.heightBox,
+            appname.text.size(22).fontFamily(bold).white.make(),
+            5.heightBox,
+            appversion.text.white.make(),
+            const Spacer(),
+            credits.text.white.fontFamily(semibold).make(),
+            30.heightBox,
+          ],
+        ),
+      ),
+    );
+  }
+}
