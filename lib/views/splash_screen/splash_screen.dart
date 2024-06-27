e62b19cda651aebd2b73a11c676018d2f88e462c
Kausar Fatima <165108114+kausar-fatima@users.noreply.github.com>
2024-06-27 06:24:29 +0500

    Updated splash_screen.dart

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -12,29 +12,53 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Image.asset(
-                icSplashBg,
-                width: 300,
-              ),
-            ),
-            20.heightBox,
-            applogoWidget(),
-            10.heightBox,
-            appname.text.size(22).fontFamily(bold).white.make(),
-            5.heightBox,
-            appversion.text.white.make(),
-            const Spacer(),
-            credits.text.white.fontFamily(semibold).make(),
-            30.heightBox,
-          ],
-        ),
+        // child: VxInlineBlock(
+        //   children: [
+        //     "VelocityX is Super".text.make().box.height(60).red500.make(),
+        //     "VelocityX increase productivity".text.make().box.red500.make(),
+        //     "VelocityX is Super".text.make().box.red500.make(),
+        //     "VelocityX increase productivity".text.make().box.red500.make(),
+        //     "VelocityX is Super".text.make().box.height(60).red500.make(),
+        //     "VelocityX increase productivity".text.make().box.red500.make(),
+        //     "VelocityX is Super".text.make().box.red500.make(),
+        //     "VelocityX increase productivity".text.make().box.red500.make()
+        //   ],
+        //   verticalDirection: VerticalDirection.up,
+        //   textBaseline: TextBaseline.ideographic,
+        // ).scrollHorizontal(),
+        child: "VelocityX"
+            .text
+            .make()
+            .card
+            .shadowColor(Colors.red)
+            .elevation(10)
+            .hexColor("#cfcfcf")
+            .make(),
       ),
+      // body: Center(
+      //   child: Column(
+      //     children: [
+      //       Align(
+      //         alignment: Alignment.topLeft,
+      //         child: Image.asset(
+      //           icSplashBg,
+      //           width: 300,
+      //         ),
+      //       ),
+      //       20.heightBox,
+      //       applogoWidget(),
+      //       10.heightBox,
+      //       appname.text.size(22).fontFamily(bold).white.make(),
+      //       5.heightBox,
+      //       appversion.text.white.make(),
+      //       const Spacer(),
+      //       credits.text.white.fontFamily(semibold).make(),
+      //       30.heightBox,
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
