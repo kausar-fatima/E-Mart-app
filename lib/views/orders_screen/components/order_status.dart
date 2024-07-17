5b2f8953dcd6c67b88daa92efce8aa7026205566
Kausar Fatima <165108114+kausar-fatima@users.noreply.github.com>
2024-07-18 04:32:52 +0500

    worked on order details

---
+++ lib/views/orders_screen/components/order_status.dart
@@ -0,0 +1,26 @@
+import 'package:e_mart_app/consts/consts.dart';
+
+Widget orderStatus({icon, color, title, showDone}) {
+  return ListTile(
+    leading: Icon(
+      icon,
+      color: color,
+    ).box.border(color: color).make(),
+    trailing: SizedBox(
+      height: 100,
+      width: 120,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "$title".text.color(darkFontGrey).make(),
+          showDone
+              ? const Icon(
+                  Icons.done,
+                  color: redColor,
+                )
+              : Container(),
+        ],
+      ),
+    ),
+  );
+}
--- lib/views/orders_screen/order_screen.dart
+++ lib/views/orders_screen/order_screen.dart
@@ -19,7 +19,38 @@ class OrderScreen extends StatelessWidget {
             } else if (snapshot.data!.docs.isEmpty) {
               return "No Orders Yet!".text.color(darkFontGrey).makeCentered();
             } else {
-              return Container();
+              var data = snapshot.data!.docs;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: "${index + 1}"
+                        .text
+                        .fontFamily(bold)
+                        .color(darkFontGrey)
+                        .xl
+                        .make(),
+                    title: data[index]['order_code']
+                        .toString()
+                        .text
+                        .color(redColor)
+                        .fontFamily(semibold)
+                        .make(),
+                    subtitle: data[index]['total_amount']
+                        .toString()
+                        .numCurrency
+                        .text
+                        .fontFamily(bold)
+                        .make(),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: darkFontGrey,
+                      ),
+                    ),
+                  );
+                },
+              );
             }
           }),
     );
